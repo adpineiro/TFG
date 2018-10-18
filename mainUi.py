c5cb242f7120e60de55d76be4cb141d963814dff
Adrian Piñeiro Perez <adpineiro@alumnos.uvigo.es>
2018-10-18 13:40:10 +0200

    Ajuste a las normas PEP

--- mainUi.py
+++ mainUi.py
@@ -2,7 +2,7 @@
 
 """
 # librerias PyQt5
-from PyQt5 import QtWidgets, QtCore,QtGui
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtCore import QRegularExpression, QTimer
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtGui import QIcon, QPixmap
@@ -12,10 +12,11 @@ import sys
 import mainwindowinterface
 import procesado_imagen
 
+
 class MainWindow(QtWidgets.QMainWindow, mainwindowinterface.Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
-        #super(MainWindow, self).__init__()
+        # super(MainWindow, self).__init__()
         self.setupUi(self)
 
         # inicializa mostrar las cámaras
@@ -26,7 +27,7 @@ class MainWindow(QtWidgets.QMainWindow, mainwindowinterface.Ui_MainWindow):
         self.__actualizar_imagen.timeout.connect(self.__imagen_actualizar)
 
     def __imagen_actualizar(self):
-        imagen = procesado_imagen.image_stack()
+        procesado_imagen.image_stack()
         pixmap = QPixmap('salida.jpg') #muestra una imagen en la label
         self.label_image1.setPixmap(pixmap)
 
